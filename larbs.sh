b3cd0b8f80cf9abb8c017d284812fa0d9985e30d
und3rdg <und3rdg@gmail.com>
2018-12-28 21:53:52 +0000

    Revert "update reposiitories"
    This reverts commit 21c1249916d98c263adeba2091afcd32b82ce7c3.

--- larbs.sh
+++ larbs.sh
@@ -20,16 +20,15 @@ while getopts ":a:r:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z ${dot_repo+x} ] && dot_repo="https://github.com/und3rdg/dot_base.git"
-[ -z ${vim_repo+x} ] && vim_repo="https://github.com/und3rdg/dot_vim.git"
-[ -z ${zsh_repo+x} ] && zsh_repo="https://github.com/und3rdg/dot_zsh.git"
-[ -z ${i3_repo+x}  ] && i3_repo="https://github.com/und3rdg/dot_i3.git"
-[ -z ${vimwiki_repo+x} ] && vimwiki_repo="https://github.com/und3rdg/dot_vimwiki.git"
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/und3rdg/dot_base.git"
+[ -z ${vimfilesrepo+x} ] && vimfilesrepo="https://github.com/und3rdg/.vim.git"
+[ -z ${zshfilesrepo+x} ] && zshfilesrepo="https://github.com/und3rdg/zsh.git"
+[ -z ${i3filesrepo+x}  ] && i3filesrepo="https://github.com/und3rdg/dot_i3.git"
 
 [ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/und3rdg/dot_install/master/progs.csv"
 [ -z ${aurhelper+x} ] && aurhelper="yay"
 
-installation_date=$(date +%Y_%m_%d-%H_%M_%S)
+install_date=$(date +%Y_%m_%d-%H_%M_%S)
 
 ###
 ### FUNCTIONS ###
@@ -130,18 +129,18 @@ systembeepoff() { dialog --infobox "Getting rid of that retarded error beep soun
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
 
 # Downlods a gitrepo $1 and places the files in $2 only overwriting conflicts
-git_put() {
+putgitrepo() {
 	dialog --infobox "Downloading and installing config files..." 4 60
 	dir=$(mktemp -d)
 	chown -R "$name":wheel "$dir"
 	sudo -u "$name" git clone --depth 1 "$1" "$dir"/gitrepo &>/dev/null &&
 	sudo -u "$name" mkdir -p "$2" &&
   [ -d $2/.git ] && chown -R "$name":wheel "$2/.git/"
-	sudo -u "$name" cp -rT "$dir"/gitrepo "$2"
+	cp -rT "$dir"/gitrepo "$2"
 }
 
 git_clone() {
-  date_folder="/home/$name/old_backup/$installation_date"
+  date_folder="/home/$name/old_backup/$install_date"
   if [ -d "$2" ] ; then
     dialog --infobox "$2 || Backup old $date_folder..." 4 80
     chown -R "$name":wheel "$2"
@@ -236,11 +235,10 @@ manualinstall $aurhelper
 installationloop
 
 # Install the dotfiles in the user's home directory
-git_put    "$dot_repo"     "/ home/$name"
-git_clone  "$vim_repo"     "/ home/$name/.vim"
-git_clone  "$zsh_repo"     "/ home/$name/.zprezto"
-git_clone  "$i3_repo"      "/ home/$name/.config/i3"
-git_clone  "$vimwiki_repo" "/ home/$name/vimwiki"
+putgitrepo "$dotfilesrepo" "/home/$name"
+git_clone "$vimfilesrepo" "/home/$name/.vim"
+git_clone "$zshfilesrepo" "/home/$name/.zprezto"
+# git_clone "$i3filesrepo" "/home/$name/.config/i3"
 
 # Install the LARBS Firefox profile in ~/.mozilla/firefox/
 # putgitrepo "https://github.com/LukeSmithxyz/mozillarbs.git" "/home/$name/.mozilla/firefox"
