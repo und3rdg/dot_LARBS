d5b070a03219a5e9e0bfc221a5601739d931131a
und3rdg <und3rdg@gmail.com>
2019-01-01 21:28:00 +0000

    update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+[ -z ${reposfile+x} ] && reposfile="https://raw.githubusercontent.com/und3rdg/dot_install/master/repos.csv"
+
+git_pull_loop() {
+	([ -f "$reposfile" ] && cp "$reposfile" /tmp/repos.csv) || curl -Ls "$reposfile" | sed '/^#/d' > /tmp/repos.csv
+	total=$(wc -l < /tmp/repos.csv)
+	while IFS=, read -r url dest; do
+    echo "--------------------------------------------"
+    echo "url"
+    echo "--------------------------------------------"
+    git -c "~/$dest" pull
+	done < /tmp/repos.csv 
+}
+
+git_pull_loop
